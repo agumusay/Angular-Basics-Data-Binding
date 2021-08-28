46f5b541cb57ec1b44eac1e4b05855c3132afa4b
Ali San Gumusay <alisan@pop-os.localdomain>
2021-08-28 21:48:46 +0200

    basics data binding

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,47 @@
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
+
+@Component({
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html",
+  styleUrls: ["./cockpit.component.css"],
+})
+export class CockpitComponent implements OnInit {
+  //@Output decorator lets child component to send data to parent component.You can define an alias by passing a string as the first argument
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  @Output("bpCreated") blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  // newServerName = "";
+  // newServerContent = "";
+
+  // @ViewChild gives you a reference to the element with the type ElementRef. You can access values by using Element.nativeElement.value
+  @ViewChild("serverContentInput", { static: true })
+  serverContentInput: ElementRef;
+  constructor() {}
+
+  ngOnInit(): void {}
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
+    });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value,
+    });
+  }
+}
